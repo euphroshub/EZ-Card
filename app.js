1a1780f7345e5d07a9cb524c4182334275986ee7
euphroshub <euphros@protonmail.com>
2021-10-24 16:29:16 -0400

    wip

--- app.js
+++ app.js
@@ -4,6 +4,8 @@
 const holesInput = document.getElementsByClassName('c-Holes');
 const parInput = document.getElementsByClassName('c-Par');
 const scoreInput = document.getElementsByClassName('c-Game');
+const appIntro = document.getElementsByClassName('appIntro');
+const holeNumer = document.getElementsByClassName("holeNumber");
 
 // Buttons
 const btnStart = document.getElementsByClassName('start');
@@ -11,7 +13,8 @@ const btnNext = document.getElementsByClassName('next');
 
 // Variables
 const par = document.getElementById("par").value;
-const holes = document.getElementById("holes").value;
+let holes = document.getElementById("holes").value;
+let currentHole = 1;
 
 // To calculate the score each holes
 let holeScore = 0;
@@ -24,6 +27,8 @@ function startGame() {
     const holesElement = holesInput[0];
     const parElement = parInput[0];
     const scoreElement = scoreInput[0]; 
+    const appIntroElement = appIntro[0];
+    const holeNumberElement = holeNumer[0];
 
     start.addEventListener('click', function (e) {
         e.preventDefault();
@@ -33,8 +38,15 @@ function startGame() {
         holesElement.classList.add('hidden');
         parElement.classList.add('hidden');
         scoreElement.classList.remove('hidden');
+        appIntroElement.classList.add('hidden');
+        holeNumberElement.classList.remove('hidden');
+
+        // Starting the game at hole number 1 and displaying it in the UI.
+        holeNumberElement.children[0].textContent = `Hole ${currentHole}`;
+
+        // Converting the par and holes value to integer numerical value.
         const par = parseInt(document.getElementById("par").value);
-        const holes = parseInt(document.getElementById("holes").value);
+        let holes = parseInt(document.getElementById("holes").value);
 
         console.log(`Holes: ${holes}`);
         console.log(`Par: ${par}`);
@@ -46,27 +58,48 @@ function startGame() {
 
 startGame();
 
+
 // Adding score value to the array
 function buildScore() {
+    // To update the hole number
+    const holeNumberElement = holeNumer[0];
+
     scoreList[holeScore] = document.getElementById("score").value;
     
-    console.log(`Element: ${scoreList[holeScore]} Added at index ${holeScore}`);
-    console.log(scoreList);
-
+    // Adding the score to the array
     holeScore++;
+    // Switching hole on each btn click
+    currentHole++;
+
+    // Reducing the number of holes left in the game
+    holes--;
+
+    // Updating the hole number displayed on the UI.
+    holeNumberElement.children[0].textContent = `Hole ${currentHole}`;
+
+    // Reseting the score input after each holes
     document.getElementById("score").value = '';
 
-    // Adding the score to the final score.
+    // Adding the holes score to the final score.
     let finalScore = 0;
 
-    for (let i = 0; i < totalScore.length; i++) {
+    for (let i = 0; i < scoreList.length; i++) {
         finalScore += parseInt(scoreList[i]);
     }
 
-    console.log(finalScore);
+    // Stopping the game if holes reach 0
+    
 
-    console.log(holes);
+    
+
+    // Logs
+    console.log(`Final score: ${finalScore}`);
+    console.log(`Holes left: ${holes}`);
+
+    console.log(`Current hole: ${currentHole}`);
+    
 }
+
 
 
 // TODO
